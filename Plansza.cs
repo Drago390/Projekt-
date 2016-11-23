4dd6644adb9146f09ad6732a046469f5fe091c38
Drago390 <jakub.zar@wp.pl>
2016-11-23 20:12:34 +0100

    Create Plansza.cs

---
+++ Plansza.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    public class Plansza
+    {
+        private int x, y, width, height;
+        private SolidBrush brush;
+        public Rectangle pRec;
+
+        public Plansza(Random randPlansza)
+        {
+            x = randPlansza.Next(0, 20) * 10;
+            y = randPlansza.Next(0, 29) * 10;
+
+            brush = new SolidBrush(Color.Black);
+
+            width = 10;
+            height = 10;
+            pRec = new Rectangle(x, y, width, height);
+
+        }
+
+        public void Miejsce(Random randPlansza)
+        {
+            x = randPlansza.Next(0, 20) * 10;
+            y = randPlansza.Next(0, 29) * 10;
+        }
+
+        public void rysowanie(Graphics paper)
+        {
+            pRec.X = x;
+            pRec.Y = y;
+            paper.FillRectangle(brush, pRec);
+        }
+    }
+}
